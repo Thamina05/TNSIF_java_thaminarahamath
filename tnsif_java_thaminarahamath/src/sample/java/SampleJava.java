89adee377d82fb2b5f59b98de1857f9ec468b063
191024 <191024@DESKTOP-2PJ3ETG>
2025-09-25 12:57:53 +0530

    commit1

---
+++ tnsif_java_thaminarahamath/src/sample/java/SampleJava.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package sample.java;
+
+/**
+ * 
+ */
+public class SampleJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
