a8e25b0ee3d1c45be4e846ce55ec5d673bf5a13c
191024 <191024@DESKTOP-2PJ3ETG>
2025-09-29 14:02:41 +0530

    commit4

---
+++ tnsif_java_thaminarahamath/src/basics/java/DoWhileDemo.java
@@ -0,0 +1,13 @@
+package basics.java;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 11;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 1);
+	}
+}
