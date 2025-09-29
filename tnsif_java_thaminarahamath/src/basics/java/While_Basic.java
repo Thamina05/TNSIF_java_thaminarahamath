332098314a16a86b9315ced05b021d10dc2813df
191024 <191024@DESKTOP-2PJ3ETG>
2025-09-29 21:40:58 +0530

    commit2

---
+++ tnsif_java_thaminarahamath/src/basics/java/While_Basic.java
@@ -0,0 +1,16 @@
+package basics.java;
+
+public class While_Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+
+	}
+
+	}
+}
