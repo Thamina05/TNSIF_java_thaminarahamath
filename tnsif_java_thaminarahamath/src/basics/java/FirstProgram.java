745ef7337d93124b175690aa133d1ebf177222fd
191024 <191024@DESKTOP-2PJ3ETG>
2025-09-29 14:05:13 +0530

    commit5

---
+++ tnsif_java_thaminarahamath/src/basics/java/FirstProgram.java
@@ -0,0 +1,12 @@
+package basics.java;
+import java.util.LinkedList ;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
