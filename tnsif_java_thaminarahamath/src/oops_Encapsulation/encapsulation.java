589b47b5b7634f4f295b5c1d01e4102bca208e77
191024 <191024@DESKTOP-2PJ3ETG>
2025-10-03 16:58:36 +0530

    commit2

---
+++ tnsif_java_thaminarahamath/src/oops_Encapsulation/encapsulation.java
@@ -0,0 +1,16 @@
+package oops_Encapsulation;
+
+public class encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Oopsconcept obj = new Oopsconcept();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+
+	}
+
+}
