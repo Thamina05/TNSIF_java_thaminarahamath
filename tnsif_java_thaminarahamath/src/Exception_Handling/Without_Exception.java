3b437e335ea70bdcbff9b9c88dfd1f8bc7be515d
191024 <191024@DESKTOP-2PJ3ETG>
2025-10-08 15:59:35 +0530

    commit2

---
+++ tnsif_java_thaminarahamath/src/Exception_Handling/Without_Exception.java
@@ -0,0 +1,15 @@
+package Exception_Handling;
+
+public class Without_Exception {
+
+	public static void main(String[] args) {
+		
+		
+				System.out.println("the program continues....");
+				int data=100/0;
+			}
+			
+
+
+	
+}
