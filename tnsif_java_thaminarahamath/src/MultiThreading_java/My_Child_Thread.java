125ba400ce02a6418c4c26e5dcebb177eac3e478
191024 <191024@DESKTOP-2PJ3ETG>
2025-10-09 16:13:30 +0530

    commit2

---
+++ tnsif_java_thaminarahamath/src/MultiThreading_java/My_Child_Thread.java
@@ -0,0 +1,24 @@
+package MultiThreading_java;
+
+public class My_Child_Thread extends Thread {  
+			@Override
+			public void run() {
+				System.out.println("Inside run() Thread is alive or not? " + this.isAlive());
+				int no = 0;
+				while (no < 4) {
+					no++;
+					System.out.println("number = " + no);
+					try {
+						sleep(500);
+
+					} catch (InterruptedException exp) {
+						System.err.println("Thread Interrupted ...");
+					}
+				}
+			}
+		
+
+
+	}
+
+
